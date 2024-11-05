a6c635a4ed42b5001e3dd5023735f1ced56613fc
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-11-05 18:34:33 +0100

    commit

--- Practica2/Ejercicio9/js/script.js
+++ Practica2/Ejercicio9/js/script.js
@@ -1,14 +1,16 @@
 document.addEventListener('DOMContentLoaded',function(){
-    const abrirButton = document.getElementById("abrir");
-    abrirButton.textContent = "Abrir nueva ventana";
-
-    abrirButton.addEventListener("click",function(){
-        let ventana = window.open("","_blank","width=300,height=200");
-        ventana.document.write("<button id='cerrar'>Cerrar ventana</button>");
-            
-        const cerrarButton = ventana.document.getElementById("cerrar");
-                cerrarButton.addEventListener("click",function(){
-                    ventana.close();
-                });
-    });
+    var acciones = {
+        abrir: () => {
+           var ventana = window.open("","_blank");
+           ventana.document.write("<button id='cerrar'>Cerrar</button>");
+           ventana.document.getElementById("cerrar").addEventListener("click",function(){
+            ventana.close();
+           });
+        }
+    }
+    function manejarEvento(event) {
+        var accion = acciones[event.target.id]
+        accion();
+    }
+    document.addEventListener("click",manejarEvento);
 });
