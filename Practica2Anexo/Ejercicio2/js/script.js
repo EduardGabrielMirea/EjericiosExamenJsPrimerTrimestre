1840af256b6a9c796fcb8dfbc5da9004cca3037e
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-11-05 19:52:04 +0100

    commit

--- Practica2Anexo/Ejercicio2/js/script.js
+++ Practica2Anexo/Ejercicio2/js/script.js
@@ -1,12 +1,18 @@
 document.addEventListener("DOMContentLoaded",function () {
-    const form = document.querySelector("form");
+    const contador = document.getElementById("contador");
+    contador.value = 0;
+    contador.readOnly = true; //readOnly para que no se pueda editar.
 
-    form.addEventListener("submit",(event)=> {
-        var dniInput = document.getElementById("dni").value;
-
-        if (dniInput.trim() === "") {
-            event.preventDefault();
-            alert("El campo dni tiene que estar completo");
+    var acciones = {
+        sumar: () => {
+            contador.value = parseInt(contador.value) + 10;
+        },
+        inicializar: () => {
+            contador.value = 0;
         }
-    });
+    };
+
+    document.getElementById("sumar").addEventListener("click", () => acciones.sumar());
+    document.getElementById("inicializar").addEventListener("click", () => acciones.inicializar());
+
 });
