27a5423977fa7747893c7e32fde875f3923c8374
Eduard Gabriel Mirea <eduardmirea00@gmail.com>
2024-11-05 19:01:21 +0100

    commit

--- Practica2Anexo/Ejercicio1/js/script.js
+++ Practica2Anexo/Ejercicio1/js/script.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded",function () {
-    const link = document.getElementById("hipervinculo");
-    
-    link.addEventListener("mouseover", (event)=>{
-        event.preventDefault();
-        alert("Estas encima del link.");
+document.addEventListener("DOMContentLoaded",function () {  
+    var links = document.querySelectorAll("a");
+
+    //El forEach hace que añadas por cada link el evento mouseover.
+    links.forEach(function (link) {     
+        link.addEventListener("mouseover",function (event) {
+            if (!(link.href === "http://google.es/")) {
+                alert("No estas encima del link");
+            } else {
+                alert("Estas encima de un link de google");
+            }
+        })
     })
 });
